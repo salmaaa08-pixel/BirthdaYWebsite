9c5491f88276cca798650e8f9c68d2019f07ffe8
salmaaa08-pixel <salmaabdulsatar22108@gmail.com>
2026-08-13 23:29:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const correctPasscode = "170806";
+const friendshipStart = new Date("2024-03-10");
+const today = new Date();
+const diffTime = today - friendshipStart;
+const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
 
+const years = Math.floor(diffDays / 365);
+const remainingDays = diffDays % 365;
+
+const counterNumber = document.getElementById("counter-number");
+counterNumber.textContent = years + " years, " + remainingDays + " days";
 const errorMsg = document.getElementById("error-msg");
 const lockScreen = document.getElementById("lock-screen");
 const celebrationPage = document.getElementById("celebration-page");
